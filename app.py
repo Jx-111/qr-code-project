948df063eaa1986572531e9036c5617c2cc7c135
Jx377 <15914078183@163.com>
2025-11-15 20:35:40 +0800

    Update app.py to fix file system issues and improve QR code generation

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 from flask import Flask, render_template, request, send_file, jsonify
 import qrcode
-import os
 from io import BytesIO
-from werkzeug.utils import secure_filename
-from flask import url_for
 
 app = Flask(__name__)
-
-# 设置二维码存储的目录
-QR_CODE_FOLDER = 'static/qr_codes'
-if not os.path.exists(QR_CODE_FOLDER):
-    os.makedirs(QR_CODE_FOLDER)
 
 # 主页路由，展示网页表单
 @app.route('/')
@@ -38,18 +30,13 @@ def generate():
     # 创建二维码图片
     img = qr.make_image(fill='black', back_color='white')
 
-    # 使用用户输入数据的前 10 个字符作为文件名，避免文件名太长
-    filename = secure_filename(f"{data[:10]}.png")
-    file_path = os.path.join(QR_CODE_FOLDER, filename)
+    # 将二维码图片保存到内存
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
 
-    # 保存二维码图片到文件系统
-    img.save(file_path)
-
-    # 使用 Flask 的 url_for 动态生成图片的 URL
-    qr_code_url = url_for('static', filename=f'qr_codes/{filename}')
-
-    # 返回二维码图片的 URL 作为 JSON
-    return jsonify({'qr_code_url': qr_code_url})
+    # 返回二维码图片
+    return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
